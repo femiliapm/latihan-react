620bea510f4a1dedcd4b66ae37daaa9e44090fb8
femiliapm <femiliaputrimayranti@gmail.com>
2021-03-22 11:35:41 +0700

    looping data for card component

---
+++ src/containers/Home/Data.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-expressions */
+const Data = [
+  {
+    title: "Tutorial HTML",
+    srcImage: ""
+  },
+  {
+    title: "Tutorial JavaScript",
+    srcImage: ""
+  },
+  {
+    title: "Tutorial Java",
+    srcImage: ""
+  },
+  {
+    title: "Tutorial React JS",
+    srcImage: ""
+  },
+  {
+    title: "Tutorial Programming",
+    srcImage: ""
+  }
+]
+
+export default Data;
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from "react";
 import Card from "../../components/Card";
 import Header from "../../components/Header";
+import Data from "./Data";
 
 class Home extends Component {
   render() {
@@ -10,12 +11,17 @@ class Home extends Component {
         <div className="App">
           <Header />
         </div>
-        <Card
-          srcImage="https://placeimg.com/200/200/nature"
-          title="Tutorial React JS"
-        />
-        <Card title="Tutorial Javascript" />
-        <Card title="Tutorial Java Spring Boot" />
+        <div className="d-flex flex-row">
+          <Card
+            srcImage="https://placeimg.com/200/200/nature"
+            title="Tutorial React JS"
+          />
+          <Card title="Tutorial Javascript" />
+          <Card title="Tutorial Java Spring Boot" />
+          {Data.map((val) => {
+            return <Card title={val.title} srcImage={val.srcImage} />;
+          })}
+        </div>
         <button className="btn btn-primary">Home</button>
       </Fragment>
     );
